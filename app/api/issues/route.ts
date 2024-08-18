3605dda8c59b402ee906aa90bdb420a827f8b424
SaeedRahmanRao <raosaeedrahmankhan@gmail.com>
2024-08-18 11:22:48 +0500

    handle form submission

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,20 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
-import {z} from "zod";
+import { z } from "zod";
 import prisma from "@/prisma/client";
 
-const createIssueSchema=z.object({
-    title:z.string().min(1).max(225),
-    description : z.string().min(1),
-})
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(225),
+  description: z.string().min(1),
+});
 
-export async function POST(request:NextRequest){
-    const body=await request.json();
-    const validation = createIssueSchema.safeParse(body);
-    if(!validation.success)
-        return NextResponse.json(validation.error.errors,{status:400})
+export async function POST(request: NextRequest) {
+  const body = await request.json();
 
-    const newIssue=await prisma.issue.create({
-        data:{title:body.title , description:body.description}
-    });
-    return NextResponse.json(newIssue,{status:201})
+  // Log incoming data
+  console.log("Incoming data:", body);
+
+  const validation = createIssueSchema.safeParse(body);
+
+  if (!validation.success) {
+    // Log validation errors
+    console.error("Validation errors:", validation.error.errors);
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
 }
