7b093dc5411bbd125088f2c1610ae09d32264264
SaeedRahmanRao <raosaeedrahmankhan@gmail.com>
2024-08-19 13:23:53 +0500

    Refactor: Organize Import statements

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -2,8 +2,7 @@ import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { Heading, Card, Flex, Box } from "@radix-ui/themes";
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
 const LoadingIssueDetailPage = () => {
   return (
     <Box className="max-w-xl">
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,14 @@
-import React from "react";
 import { Table } from "@radix-ui/themes";
 //import Link from "next/link";
 import prisma from "@/prisma/client";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+//import IssueStatusBadge from "../components/IssueStatusBadge";
+import { IssueStatusBadge, Link } from "@/app/components";
+//import delay from "delay";
+//import Link from "../components/Link";
 import IssueActions from "./IssueActions";
-import Link from "../components/Link";
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(1000);
+  //await delay(1000);
   return (
     <div>
       <IssueActions />
